6fbf5c0fe967650e018520ee068dfab244a43338
Yi Zhao <zhao1max@gmail.com>
2016-05-05 08:27:48 -0400

    server update

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://159.203.42.55/',
+        url: 'http://blog.yizhao.me/',
         mail: {},
         database: {
             client: 'sqlite3',
